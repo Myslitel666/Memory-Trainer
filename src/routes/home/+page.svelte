c0c8178c3d39c33335fc7ade1d8f815b8b692366
Myslitel666 <101917322+Myslitel666@users.noreply.github.com>
2025-03-21 16:02:46 +0500

    Update +page.svelte

--- src/routes/home/+page.svelte
+++ src/routes/home/+page.svelte
@@ -64,7 +64,7 @@
   <div class="mgn-top">
     <span style:visibility={shTxtAr}>Count: {cntChr}</span>
     <span style:visibility={shTxtAr} style:margin-left="1.5rem"
-      >Your Record: <span style:color="green">{record}</span></span
+      >Your Record2: <span style:color="green">{record}</span></span
     >
   </div>
   <div class="mgn-top">

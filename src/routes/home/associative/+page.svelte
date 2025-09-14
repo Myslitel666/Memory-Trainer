9de58f9a79c8b3c4f8ff150e2b69e8241a2fd5ff
Myslitel666 <tretyakov_02@bk.ru>
2025-09-14 14:16:24 +0500

    i

--- src/routes/home/associative/+page.svelte
+++ src/routes/home/associative/+page.svelte
@@ -248,97 +248,63 @@
     </div>
     <div class="mgn-top">
       <div style:display="flex" style:flex-direction="column">
-        {#if memoryItems === "Numbers and Letters" || memoryItems === "Words"}
-          <div
-            style:margin-bottom="1rem"
-            style:padding-left="0.83rem"
-            style:padding-right="0.97rem"
-            style:width="100vw"
-            style:max-width="27.5rem"
-            style:box-sizing="border-box"
-          >
-            <TextField
-              bind:this={textFieldElement}
-              bind:value={inputStr}
-              onkeydown={(e) => {
-                if (e.key === "Enter") {
-                  onEnterClick();
-                }
-              }}
-              label="Enter the String"
-              disabled={!isHidden}
-              width="100%"
-            />
-            <Button
-              marginTop="0.66rem"
-              width="100%"
-              isPrimary={true}
-              onClick={onEnterClick}
-              disabled={!isHidden}
-            >
-              Next Level
-            </Button>
+        {#each buttons as buttonLine}
+          <div style:display="flex" style:flex-direction="row">
+            {#each buttonLine as button}
+              {#if button != "Bs" && button != "En"}
+                <ButtonBox
+                  marginRight={Number(button) % 3 === 0 && Number(button) !== 0
+                    ? ""
+                    : "0.75rem"}
+                  marginBottom={button === "0" ? "" : "0.75rem"}
+                  onClick={(event: MouseEvent) => {
+                    onNumbClick(event, button);
+                  }}
+                >
+                  {button}
+                </ButtonBox>
+              {:else if button == "Bs"}
+                <ButtonBox
+                  marginRight="0.75rem"
+                  isPrimary={true}
+                  onClick={onBackClick}
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    fill="currentColor"
+                    class="size-6"
+                    height="4rem"
+                    width="4rem"
+                  >
+                    <path
+                      fill-rule="evenodd"
+                      d="M2.515 10.674a1.875 1.875 0 0 0 0 2.652L8.89 19.7c.352.351.829.549 1.326.549H19.5a3 3 0 0 0 3-3V6.75a3 3 0 0 0-3-3h-9.284c-.497 0-.974.198-1.326.55l-6.375 6.374ZM12.53 9.22a.75.75 0 1 0-1.06 1.06L13.19 12l-1.72 1.72a.75.75 0 1 0 1.06 1.06l1.72-1.72 1.72 1.72a.75.75 0 1 0 1.06-1.06L15.31 12l1.72-1.72a.75.75 0 1 0-1.06-1.06l-1.72 1.72-1.72-1.72Z"
+                      clip-rule="evenodd"
+                    />
+                  </svg>
+                </ButtonBox>
+              {:else if button == "En"}
+                <ButtonBox isPrimary={true} onClick={onEnterClick}>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    fill="currentColor"
+                    class="size-6"
+                    height="4rem"
+                    width="4rem"
+                  >
+                    <path
+                      fill-rule="evenodd"
+                      d="M16.5 3.75a1.5 1.5 0 0 1 1.5 1.5v13.5a1.5 1.5 0 0 1-1.5 1.5h-6a1.5 1.5 0 0 1-1.5-1.5V15a.75.75 0 0 0-1.5 0v3.75a3 3 0 0 0 3 3h6a3 3 0 0 0 3-3V5.25a3 3 0 0 0-3-3h-6a3 3 0 0 0-3 3V9A.75.75 0 1 0 9 9V5.25a1.5 1.5 0 0 1 1.5-1.5h6Zm-5.03 4.72a.75.75 0 0 0 0 1.06l1.72 1.72H2.25a.75.75 0 0 0 0 1.5h10.94l-1.72 1.72a.75.75 0 1 0 1.06 1.06l3-3a.75.75 0 0 0 0-1.06l-3-3a.75.75 0 0 0-1.06 0Z"
+                      clip-rule="evenodd"
+                    />
+                  </svg>
+                </ButtonBox>
+              {/if}
+            {/each}
           </div>
-        {:else}
-          {#each buttons as buttonLine}
-            <div style:display="flex" style:flex-direction="row">
-              {#each buttonLine as button}
-                {#if button != "Bs" && button != "En"}
-                  <ButtonBox
-                    marginRight={Number(button) % 3 === 0 &&
-                    Number(button) !== 0
-                      ? ""
-                      : "0.75rem"}
-                    marginBottom={button === "0" ? "" : "0.75rem"}
-                    onClick={(event: MouseEvent) => {
-                      onNumbClick(event, button);
-                    }}
-                  >
-                    {button}
-                  </ButtonBox>
-                {:else if button == "Bs"}
-                  <ButtonBox
-                    marginRight="0.75rem"
-                    isPrimary={true}
-                    onClick={onBackClick}
-                  >
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      viewBox="0 0 24 24"
-                      fill="currentColor"
-                      class="size-6"
-                      height="4rem"
-                      width="4rem"
-                    >
-                      <path
-                        fill-rule="evenodd"
-                        d="M2.515 10.674a1.875 1.875 0 0 0 0 2.652L8.89 19.7c.352.351.829.549 1.326.549H19.5a3 3 0 0 0 3-3V6.75a3 3 0 0 0-3-3h-9.284c-.497 0-.974.198-1.326.55l-6.375 6.374ZM12.53 9.22a.75.75 0 1 0-1.06 1.06L13.19 12l-1.72 1.72a.75.75 0 1 0 1.06 1.06l1.72-1.72 1.72 1.72a.75.75 0 1 0 1.06-1.06L15.31 12l1.72-1.72a.75.75 0 1 0-1.06-1.06l-1.72 1.72-1.72-1.72Z"
-                        clip-rule="evenodd"
-                      />
-                    </svg>
-                  </ButtonBox>
-                {:else if button == "En"}
-                  <ButtonBox isPrimary={true} onClick={onEnterClick}>
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      viewBox="0 0 24 24"
-                      fill="currentColor"
-                      class="size-6"
-                      height="4rem"
-                      width="4rem"
-                    >
-                      <path
-                        fill-rule="evenodd"
-                        d="M16.5 3.75a1.5 1.5 0 0 1 1.5 1.5v13.5a1.5 1.5 0 0 1-1.5 1.5h-6a1.5 1.5 0 0 1-1.5-1.5V15a.75.75 0 0 0-1.5 0v3.75a3 3 0 0 0 3 3h6a3 3 0 0 0 3-3V5.25a3 3 0 0 0-3-3h-6a3 3 0 0 0-3 3V9A.75.75 0 1 0 9 9V5.25a1.5 1.5 0 0 1 1.5-1.5h6Zm-5.03 4.72a.75.75 0 0 0 0 1.06l1.72 1.72H2.25a.75.75 0 0 0 0 1.5h10.94l-1.72 1.72a.75.75 0 1 0 1.06 1.06l3-3a.75.75 0 0 0 0-1.06l-3-3a.75.75 0 0 0-1.06 0Z"
-                        clip-rule="evenodd"
-                      />
-                    </svg>
-                  </ButtonBox>
-                {/if}
-              {/each}
-            </div>
-          {/each}
-        {/if}
+        {/each}
       </div>
     </div>
     <div class="mgn-top">
